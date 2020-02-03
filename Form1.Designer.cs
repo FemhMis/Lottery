0e7d75ed4c32d54b78037aaccefe3ba27fd912ce
FemhMis <FemhMis@Gmail.com>
2020-02-03 21:55:50 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(933, 63);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(587, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(482, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "鎖定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(464, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.panel2.Size = new System.Drawing.Size(933, 275);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(10, 10);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(913, 255);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 338);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    public partial class Form1 : Form
+    {
+        private Gioble oData = new Gioble() { };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == 13)
+            {
+                if (CheckWinListSignificant(textBox1.Text))
+                    LockBtn(true);
+                else
+                    LockBtn(false);
+            }
+            else if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8 & (int)e.KeyChar != 44 & (int)e.KeyChar != 127)
+                e.Handled = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (oData.isLock)
+                    LockBtn(false);
+                if (CheckWinListSignificant(textBox1.Text))
+                    LockBtn(true);
+                else
+                    LockBtn(false);
+            }
+            catch (Exception _sEx)
+            {
+                MessageBox.Show(_sEx.Message);
+            }
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == 13)
+            {
+                e.Handled = true;
+                string sStr = textBox2.Text;
+                string sVal = "";
+                List<string> oList1 = sStr.Split(new string[] {"\r\n" }, StringSplitOptions.None).ToList();
+                foreach (string sTMP in oList1)
+                {
+                    string sTMP2 = sTMP.IndexOf(";") != -1 ? sTMP.Substring(0, sTMP.IndexOf(";")) : sTMP;
+                    if (CheckListSignificant(sTMP2))
+                    {
+                        sVal += sTMP2 + ";檢核成功，" + ReportResult(sTMP2) + "\r\n";
+                    }
+                    else
+                    {
+                        sVal += sTMP2 + ";檢核失敗，不做判斷" + "\r\n";
+                    }
+                }
+                textBox2.Text = sVal;
+                textBox2.SelectionStart = textBox2.Text.Length;
+                textBox2.SelectionLength = textBox2.Text.Length;
+            }
+            else if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8 & (int)e.KeyChar != 44 & (int)e.KeyChar != 127)
+                e.Handled = true;
+        }
+        void LockBtn(Boolean _Val)
+        {
+            if (_Val)
+            {
+                label1.Text = textBox1.Text;
+                textBox2.Text = "";
+                button1.Text = "解鎖";
+                textBox1.Enabled = false;
+                textBox2.Enabled = true;
+                oData.isLock = true;
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                button1.Text = "鎖定";
+                textBox1.Enabled = true;
+                textBox2.Enabled = false;
+                oData = new Gioble() { };
+            }
+        }
+        string ReportResult(string _sVal)
+        {
+            List<Int32> oList = _sVal.Split(new string[] { "," }, StringSplitOptions.None).ToList().Select(x => Convert.ToInt32(x)).ToList();
+            List<Int32> oResult = oData.oArr.Where(x=> oList.Contains(x)).ToList();
+            if (oResult.Count == 3)
+                return "重複三個數字，恭喜你有中獎。";
+            else if (oResult.Count == 4)
+                return "重複四個數字，恭喜你中獎。";
+            else if (oResult.Count == 5)
+                return "重複五個數字，恭喜您中大獎。";
+            else if (oResult.Count == 6)
+                return "重複六個數字，你該搬家了。";
+            else if (oResult.Count == 7)
+                return "重複七個數字，該看眼科了，樂透不會有七碼數字。";
+            else
+                return "銘謝惠顧，下次再加油";
+        }
+        Boolean CheckListSignificant(string _sVal, Boolean _First = false)
+        {
+            List<Int32> oList = new List<Int32>() { };
+            List<string> oTMP = new List<string>() { };
+            if (_sVal.Length == 0)
+            {
+                label1.Text = "您沒有輸入數字";
+                return false;
+            }
+            string sVal = _sVal.Replace(" ", "");
+            if (sVal.Length == 0)
+            {
+                label1.Text = "您沒有輸入數字或您輸入的都是空白";
+                return false;
+            }
+            oTMP = sVal.Split(new string[] { "," }, StringSplitOptions.None).ToList();
+            if (oTMP.Exists(x => isNum(x) == false))
+            {
+                label1.Text = "您輸入的並非全部都是數字，請確認是否有空白或非數字";
+                return false;
+            }
+            oList = oTMP.Select(x => Convert.ToInt32(x)).ToList();
+            if (oList.Exists(x => x >= 50 || x <= 0))
+            {
+                label1.Text = "您輸入的數字『大於49』或是『小於1』";
+                return false;
+            }
+            if (_First)
+            {
+                if (oList.Count != 7)
+                {
+                    label1.Text = "您輸入的數字不滿七組(樂透預設是七組數字)";
+                    return false;
+                }
+                if (oList.GroupBy(x => x).ToList().Count < 7)
+                {
+                    label1.Text = "您輸入的數字有重副";
+                    return false;
+                }
+                oData = new Gioble() { isLock = true, oArr = oList };
+            }
+            else
+            {
+                if (oList.Count != 6)
+                {
+                    label1.Text = "您輸入的數字不等於六組(樂透預設是六組數字)";
+                    return false;
+                }
+                if (oList.GroupBy(x => x).ToList().Count < 6)
+                {
+                    label1.Text = "您輸入的數字有重副";
+                    return false;
+                }
+            }
+            return true;
+        }
+        Boolean CheckWinListSignificant(string _sVal)
+        {
+            if (CheckListSignificant(_sVal, true))
+            {
+                oData = new Gioble() { isLock = true, oArr = _sVal.Split(new string[] { "," }, StringSplitOptions.None).ToList().Select(x => Convert.ToInt32(x)).ToList() };
+                return true;
+            }
+            else
+                return false;
+        }
+        Boolean isNum(string _sVal)
+        {
+            Int32 x = 0;
+            if (Int32.TryParse(_sVal, out x) == false)
+                return false;
+            else
+                return true;
+        }
+        class Gioble
+        {
+            public Gioble()
+            {
+                isLock = false;
+                oArr = new List<int>() { };
+            }
+            public Boolean isLock { get; set; }
+            public List<Int32> oArr { get; set; }
+        }
+
+    }
+}
